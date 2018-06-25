d8d02c77ae50cd1780aacd03201cbeea91b7abb6
Juan Magana <juan.magana11@gmail.com>
2018-06-25 13:48:28 -0400

    First Commit

---
+++ client/src/utils/API.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const api= {
+
+  getArticles: function() {
+    return axios.get("/api/articles");
+  },
+  deleteArticle: function(id) {
+    return axios.delete("/api/articles/" + id);
+  },
+  saveArticle: function(articleObj) {
+    return axios.post("/api/articles", articleObj);
+  },
+  getRecipes: function(topic, startYear, endYear) {
+    const authKey = "4691658a9ca94e2aa90f5dd668d07510";
+    const queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
+    authKey + "&q=" + topic + "&begin_date=" + startYear + "&end_date=" + endYear;
+    console.log(queryURL);
+    return axios.get(queryURL);
+  }
+ 
+};
+
+export default api;
